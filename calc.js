7ad389b2d21108698808b0aeb7e7aa77c02b4763
dacong <2540740154@qq.com>
2017-10-19 23:19:07 +0800

    修改外观,部分样式

--- calc.js
+++ calc.js
@@ -1,19 +1,39 @@
 function showError(message) {
+	if(message != "") {
+		document.getElementById('output_field').textContent = "0";
+		document.getElementById('input_field').textContent = "";
+
+	}
   document.getElementById('error').textContent = message;
 }
 
 function Append(data) {
 	showError("");
+	if(document.getElementById('input_field').textContent != "") {
+		document.getElementById('input_field').textContent = "";
+		if(!isNaN(parseInt(data)))
+			Clear();
+	}
+
 	var message = document.getElementById('output_field').textContent;
 	if(message == "0"&&data != "."&&data != "*"&&data != "+"&&data != "/"&&data != "-")
 		document.getElementById('output_field').textContent = data;
-	else
+	else if(message != ""&&data == "."&&isNaN(parseInt(message[message.length-1]))){
+		document.getElementById('output_field').textContent += "0.";
+	}
+	else 
 		document.getElementById('output_field').textContent += data;
 }
 
 function Delete() {
 	showError("");
 	var message = document.getElementById('output_field').textContent;
+	if(message == "0") 
+		return;
+	else if(message.substring(0, message.length - 1) == "") {
+		document.getElementById('output_field').textContent = "0";
+		return;
+	}
 	document.getElementById('output_field').textContent = 
 		message.substring(0, message.length - 1);
 }
@@ -25,22 +45,38 @@ function Clear() {
 
 function Equal() {
 	var message = document.getElementById('output_field').textContent;
+	document.getElementById('input_field').textContent = message + "=";
+
+	for(var i = 0; i < message.length - 1; i++) {
+		if(message[i] == "*"&&message[i+1] == "*") {
+			showError(message + " 不是合法算式!!");
+			return;
+		}
+		else if(message[i] == "/"&&message[i+1] == "/") {
+			showError(message + " 不是合法算式!!");
+			return;
+		}
+	}
+
 	var result;
 	try {
-		eval("result = " + message);
+		eval("result = " + message );
 	}
 	catch (error) {
 		showError(message + " 不是合法算式!!");
 		return;
 	}
 	
-	if(result == undefined || result == NaN || result == Infinity)
+	if(result == undefined || isNaN(result) || result == Infinity) {
 		showError(message + " 不是合法算式!!");
-	else 
+	}
+	else {
     document.getElementById('output_field').textContent = result;
+	}
 }
 
 window.onload = function() {
+	Clear();
 	var buttons = document.getElementsByTagName("button");
   for(var i in buttons) {
     buttons[i].onclick = function() {
